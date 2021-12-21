beb2e95deb8c0ae288ccda69cf86a4d6b048fced
Harshana Madhushanka <Harshana.m@lalco.la>
2021-12-21 22:26:19 +0530

    #header component with menu items

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import Header from './components/header/header.component';
+
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/shop' element={<ShopPage/>}></Route>
